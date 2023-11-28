448cf975dfeb46330bc258e24730974af9685e59
Rumana K <139933078+rumana-cse2023-pu@users.noreply.github.com>
2023-11-29 03:53:59 +0600

    Add files via upload

---
+++ lcm-recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    printf("The LCM of %d and %d is: %d\n", num1, num2, lcm(num1, num2));
+
+    return 0;
+}
